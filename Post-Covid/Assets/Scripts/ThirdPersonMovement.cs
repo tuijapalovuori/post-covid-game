da8be8403fe2dd05ad908953f55fd55405e4d732
Tuija Palovuori <78215258+tuijapalovuori@users.noreply.github.com>
2022-02-24 14:16:10 +0200

    Began camera and movement
    Began third person camera and movement development. Installed Cinemachine package for the camera

---
+++ Post-Covid/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+    }
+}
