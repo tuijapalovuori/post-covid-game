7201fb7324f9fe4c369da385b8a824c90f3bda8d
Tuija Palovuori <78215258+tuijapalovuori@users.noreply.github.com>
2022-03-09 15:16:33 +0200

    Added gravity and jump to player
    Also added slope to test playground to test gravity

--- Post-Covid/Assets/Scripts/ThirdPersonMovement.cs
+++ Post-Covid/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,9 +8,12 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
+    public float jumpSpeed = 6f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    private float ySpeed = 0f;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +22,26 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        // Gravity
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        // If character is grounded
+        if (controller.isGrounded) {
+
+            // This would be 0, but something with isGrounded is weird and it is fixed by this.
+            // Source: https://www.youtube.com/watch?v=ynh7b-AUSPE
+            // Note: source used 0.5f, but it did not remove the problem entirely - I think it's
+            // down to how big the character is. Changing jumpSpeed did not appear to break this.
+            ySpeed = -0.8f;
+
+            // If jump button pressed
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                ySpeed = jumpSpeed;
+            }
+
+        }
+
+        // If there is control input
         if (direction.magnitude >= 0.1f) {
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -26,7 +49,17 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            Vector3 moveDirScaled = moveDir.normalized * speed;
+
+            // Add y component
+            moveDirScaled.y = ySpeed;
+
+            controller.Move(moveDirScaled * Time.deltaTime);
+        } else {
+
+            // Only yspeed
+            controller.Move(new Vector3(0f, ySpeed, 0f) * Time.deltaTime);
         }
 
     }
