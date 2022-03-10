7a2c78ce8331c773ff3c7fee12603be5f34a8d63
Tuija Palovuori <78215258+tuijapalovuori@users.noreply.github.com>
2022-03-10 21:45:36 +0200

    Revert "Merge branch 'tuija' into entity_test_tuija"
    This reverts commit a219a29a707649266838176fcba95f5cca263126, reversing
    changes made to e3fa5183aaa6928f7555238dac064f6b152a1390.

--- Post-Covid/Assets/Scripts/TestInteractable.cs
+++ Post-Covid/Assets/Scripts/TestInteractable.cs
@@ -4,31 +4,8 @@ using UnityEngine;
 
 // Concrete interactable used for testing.
 
-public class TestInteractable : Interactable {
-
-    [SerializeField]
-    public List<string> dialogue;
-
-    private DialogueBuffer dialogueBuffer;
-
-    private void Awake() {
-
-        // Find dialogue buffer
-
-        GameObject dialogueBufferGO = GameObject.FindWithTag("DialogueBuffer");
-
-        if (dialogueBufferGO == null) {
-            Debug.LogError("TestInteractable.Awake: DialogueBuffer's GO could not be found.");
-        }
-
-        dialogueBuffer = dialogueBufferGO.GetComponent<DialogueBuffer>();
-
-        if (dialogueBuffer == null) {
-            Debug.LogError("TestInteractable.Awake: DialogueBuffer component could not be found from DialogueBuffer's GO.");
-        }
-
-    }
-
+public class TestInteractable : Interactable
+{
     public override void InteractionAreaEntered() {
         InteractionMaster.ApplyForFocus(this);
     }
@@ -50,8 +27,8 @@ public class TestInteractable : Interactable {
             return;
         }
 
-        // Check for E key (if inputs allowed)
-        if ( Input.GetKeyDown(KeyCode.E) && ( GameState.GetCurrentState() == GAMESTATE.PLAYING) ) {
+        // Check for E key
+        if (Input.GetKeyDown(KeyCode.E)) {
             Debug.Log("E key pressed!");
             TriggerInteraction();
         }
@@ -59,7 +36,6 @@ public class TestInteractable : Interactable {
 
     protected override void TriggerInteraction() {
         Debug.Log("Interaction triggered!");
-
-        dialogueBuffer.GiveLines(dialogue);
+        // TODO functionality
     }
 }
